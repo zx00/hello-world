bea74df364fe71cec950099fa1ba99f836ba65dd
zx00 <1930159706@qq.com>
2021-09-16 22:26:20 +0800

    1

---
+++ pyTest/DAY2/dataType.py
@@ -0,0 +1,40 @@
+#Nunmber
+#int float bool complex
+#int
+a = 1 #赋值
+print("int a=",a)
+a = 3 #重新赋值
+print("int a=",a)
+del a #删除数字对象索引
+#print("del int a= ",a)
+b =0xA0F #16进制
+c = 0o37
+print("16进制=",b,"8进制=",c)
+
+#float
+
+a =0.1
+print("float= ",a)
+#转换
+b=int(a)
+print("转换",b)
+
+#complex
+a = -.6545+0J
+print("complex=",a)
+
+#calculation
+
+a = 5
+print(a)
+a+=1
+print(a)
+a-=1
+print(a)
+a*=1
+print(a)
+a*=2
+print(a)
+
+
+# and or not
