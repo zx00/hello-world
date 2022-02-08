0479dfa2f6de5599ffb65cc210b7f95a8730d41e
zx00 <1930159706@qq.com>
2022-02-08 18:53:54 +0800

    1

--- pyTest/DAY2/dataStruct.py
+++ pyTest/DAY2/dataStruct.py
@@ -1,5 +1,22 @@
 #元组 tuple 类似无法修改的数组 不同元素
 tuple_name =('apple','banana','grape','orange')
+
+#1.查看
+print (tuple_name[0])
+print (tuple_name[0:3])
+#2.增加
+#3.修改
+tup1=(1,"aaa")
+tup2=("bb",22)
+#元组无法修改
+# tup1[0]=521
+print (tup1+tup2)
+#4.删除
+# 元素无法删除，只能删除元组
+del tup1
+# print (tup1)
+
+
 #列表 list 类型数组 相同元素 可修改 remove append
 list =['apple','banana','grage','orange']
 #循环遍历
@@ -20,5 +37,6 @@ list[0]='a'
 # allList(list)
 #2.4读取元素
 print (list[0])
+print (list[1:3])
 #字典 键值对 
 dict ={"a":"aa"}
